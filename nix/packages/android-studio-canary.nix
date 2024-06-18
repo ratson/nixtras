c0dbef6882d174bebf3a00a23bd3b07fcd98ce75
Ratson <github@ratson.name>
2024-06-18 22:41:14 +0800

    feat: update android-studio-canary to v2024.1.2.6

--- nix/packages/android-studio-canary.nix
+++ nix/packages/android-studio-canary.nix
@@ -1,18 +1,19 @@
-{ inputs, callPackage, makeFontsConf, gnome2, buildFHSEnv, tiling_wm ? false, ... }:
+{ inputs, callPackage, makeFontsConf, gnome2, buildFHSEnv, tiling_wm ? false
+, ... }:
 
 let
-  mkStudio = opts: callPackage (import "${inputs.nixpkgs}/pkgs/applications/editors/android-studio/common.nix" opts) {
-    fontsConf = makeFontsConf {
-      fontDirectories = [ ];
-    };
-    inherit (gnome2) GConf gnome_vfs;
-    inherit buildFHSEnv;
-    inherit tiling_wm;
-  };
-in
-mkStudio {
+  mkStudio = opts:
+    callPackage (import
+      "${inputs.nixpkgs}/pkgs/applications/editors/android-studio/common.nix"
+      opts) {
+        fontsConf = makeFontsConf { fontDirectories = [ ]; };
+        inherit (gnome2) GConf gnome_vfs;
+        inherit buildFHSEnv;
+        inherit tiling_wm;
+      };
+in mkStudio {
   channel = "canary";
   pname = "android-studio-canary";
-  version = "2024.1.2.5";
-  sha256Hash = "sha256-bhhbf5BlPgGrtxWg+1j8Uy2bS7NrOxgGA13DELgmDJY=";
+  version = "2024.1.2.6";
+  sha256Hash = "sha256-E6wfpLh4qXfGN6M3q0vMtHQXTVu30ZAZUIkKMh6WPcs=";
 }
