824a544d6b34064aaf3b9487fe60858bea0933eb
Ratson <github@ratson.name>
2024-06-22 10:11:56 +0800

    feat: update vtsls to v0.2.4

--- nix/packages/vtsls.nix
+++ nix/packages/vtsls.nix
@@ -1,20 +1,23 @@
 { buildNpmPackage, fetchurl, ... }:
 
-buildNpmPackage rec {
-  pname = "vtsls";
-  version = "0.2.3";
-  src = fetchurl {
-    url = "https://registry.npmjs.org/@vtsls/language-server/-/language-server-${version}.tgz";
-    hash = "sha256-VnMTKUqWpLxntV7RLU6Hsfet4XrdJhOrIjI3QcYpt6w=";
-  };
-
+let
+  version = "0.2.4";
   lockfile = fetchurl {
     url = "https://github.com/ratson/npm-lockfiles/raw/main/@vtsls/language-server/${version}/package-lock.json";
-    hash = "sha256-YuSINxKEpqFl7m4z7B9hnHhuhv4V84y6wMeMIANlcmY=";
+    hash = "sha256-W5rKAT0dnGhvvTKASeoDscH84bKsKqgnF05idwejicI=";
+  };
+in
+buildNpmPackage rec {
+  inherit version;
+
+  pname = "vtsls";
+  src = fetchurl {
+    url = "https://registry.npmjs.org/@vtsls/language-server/-/language-server-${version}.tgz";
+    hash = "sha256-8bStgXGc1zVngAmNZgyxoeiJ9Ybq4t0mXu1NYft7H+0=";
   };
 
   dontNpmBuild = true;
-  npmDepsHash = "sha256-NcEooikviSZyGAvJs2pp4kSVZLoz8eGEuL3GWZ/S5Is=";
+  npmDepsHash = "sha256-cwzkSqHnDENVytMOgQrQNe823z0R8M8pQ6/qJ+Abnag=";
 
   postPatch = ''
     cp "${lockfile}" ./package-lock.json
