67856eed4c5c2112ffdc2a3012d05b2e55fccf0c
Ratson <github@ratson.name>
2024-06-22 09:54:08 +0800

    feat: update release-it to v17.4.0

--- nix/packages/release-it.nix
+++ nix/packages/release-it.nix
@@ -1,20 +1,24 @@
 { buildNpmPackage, fetchurl, ... }:
 
-buildNpmPackage rec {
+let
   pname = "release-it";
-  version = "17.3.0";
-  src = fetchurl {
-    url = "https://registry.npmjs.org/${pname}/-/${pname}-${version}.tgz";
-    hash = "sha256-T6tPexYwXQCCsbwo3eI+WvQ0RYmxbzoniQSAe9Ygvr0=";
-  };
-
+  version = "17.4.0";
   lockfile = fetchurl {
     url = "https://raw.githubusercontent.com/release-it/release-it/${version}/package-lock.json";
-    hash = "sha256-KUqRl4jBnCdMelMZwSb1brSBvPPhLQXWH+az+DODEY0=";
+    hash = "sha256-hdWlsepkKqW2mmeTY5AV0Bi8B4nOSZGuYDv7syvZMPs=";
+  };
+in
+buildNpmPackage {
+  inherit pname version;
+
+  src = fetchurl {
+    url = "https://registry.npmjs.org/${pname}/-/${pname}-${version}.tgz";
+    hash = "sha256-hCIc/R/cCXwL5jYvi/xLbNK7Pjh/cQWXGQClMF95gGA=";
   };
 
   dontNpmBuild = true;
-  npmDepsHash = "sha256-vE/vWFe3fpdwhUGHKydXbd461wzBAT8w7SjlJtzXmE4=";
+  npmDepsHash = "sha256-h8xWH66QEix2VQfN2rpVXsnllZDFIeGSNYfxiLJsrwA=";
+  npmFlags = [ "--legacy-peer-deps" ];
 
   postPatch = ''
     cp "${lockfile}" ./package-lock.json
