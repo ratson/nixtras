adac06284054a48e1840f78b96a10a6ae35432b9
Ratson <github@ratson.name>
2024-06-25 21:10:21 +0800

    feat: update json-schema-to-typescript to v14.0.5

--- nix/packages/json-schema-to-typescript.nix
+++ nix/packages/json-schema-to-typescript.nix
@@ -1,16 +1,20 @@
 { buildNpmPackage, fetchurl, ... }:
 
-buildNpmPackage rec {
+let
   pname = "json-schema-to-typescript";
-  version = "14.0.4";
+  version = "14.0.5";
+  rev = "89a7b326ef18c75ad801ca93c780df8fd69ab644";
+  lockfile = fetchurl {
+    url = "https://github.com/bcherny/json-schema-to-typescript/raw/${rev}/package-lock.json";
+    hash = "sha256-Tljs6pJ21lw1y+kgzusH4LYN8nbRm+qbfrs35T/sfn0=";
+  };
+in
+buildNpmPackage {
+  inherit pname version;
+
   src = fetchurl {
     url = "https://registry.npmjs.org/${pname}/-/${pname}-${version}.tgz";
-    hash = "sha256-0nM4XHkCbxs93jWP1b5+A0/MFjORc+NLRUBK8uqmbrw=";
-  };
-
-  lockfile = fetchurl {
-    url = "https://github.com/bcherny/json-schema-to-typescript/raw/master/package-lock.json";
-    hash = "sha256-Tljs6pJ21lw1y+kgzusH4LYN8nbRm+qbfrs35T/sfn0=";
+    hash = "sha256-ME4KZ17QY2F5fJucb7txY71wz0CF2w6rdnbl7qLPl08=";
   };
 
   dontNpmBuild = true;
