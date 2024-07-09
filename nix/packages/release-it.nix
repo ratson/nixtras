8eb37dc73bee466bdb76ebf03430cdb807f22fd6
Ratson <github@ratson.name>
2024-07-10 05:41:55 +0800

    feat: update release-it to v17.5.0

--- nix/packages/release-it.nix
+++ nix/packages/release-it.nix
@@ -2,10 +2,10 @@
 
 let
   pname = "release-it";
-  version = "17.4.0";
+  version = "17.5.0";
   lockfile = fetchurl {
     url = "https://raw.githubusercontent.com/release-it/release-it/${version}/package-lock.json";
-    hash = "sha256-hdWlsepkKqW2mmeTY5AV0Bi8B4nOSZGuYDv7syvZMPs=";
+    hash = "sha256-GTUu75yYFivefGyjyLUlAhUUdaCfy8yuWPK0Iq8fkq8=";
   };
 in
 buildNpmPackage {
@@ -13,11 +13,11 @@ buildNpmPackage {
 
   src = fetchurl {
     url = "https://registry.npmjs.org/${pname}/-/${pname}-${version}.tgz";
-    hash = "sha256-hCIc/R/cCXwL5jYvi/xLbNK7Pjh/cQWXGQClMF95gGA=";
+    hash = "sha256-UmUe498YyU5AZCirySFJ1lft2/GfeyZjgvurRSm9Wv4=";
   };
 
   dontNpmBuild = true;
-  npmDepsHash = "sha256-h8xWH66QEix2VQfN2rpVXsnllZDFIeGSNYfxiLJsrwA=";
+  npmDepsHash = "sha256-FLG/xUWO84sUV1gcarvywA9i/bos6Btm/B0ejTrVW2I=";
   npmFlags = [ "--legacy-peer-deps" ];
 
   postPatch = ''
