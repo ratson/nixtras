dc02b95f863616362512839c14ced050c6807012
Ratson <github@ratson.name>
2024-11-09 19:56:47 +0800

    feat(vtsls): update to v0.2.6

--- nix/packages/vtsls.nix
+++ nix/packages/vtsls.nix
@@ -2,18 +2,18 @@
 
 let
   pname = "vtsls";
-  version = "0.2.4";
+  version = "0.2.6";
 in
 buildNpmPackage (outputs.lib.npmArgs {
   inherit fetchurl lib pname version;
 
   package = "@vtsls/language-server";
 
-  hash = "sha256-8bStgXGc1zVngAmNZgyxoeiJ9Ybq4t0mXu1NYft7H+0=";
-  json.hash = "sha256-aLtmBxcs053WGnKbxtRsHKy8XclsXkgrryq6EgWsASs=";
-  lock.hash = "sha256-W5rKAT0dnGhvvTKASeoDscH84bKsKqgnF05idwejicI=";
+  hash = "sha256-5fxQvKUzgBox66qumsOtSTh7PbSn6e/ovTOsDP8BWLY=";
+  json.hash = "sha256-g8VjYT7qokQjcUjxXC13M9Eq5POz4g8yLaK6o7ncMIk=";
+  lock.hash = "sha256-j5ylmXwpr6Gr1vUvi9whn87lgNXI3+zyVo0r1j1HqiY=";
 
-  npmDepsHash = "sha256-cwzkSqHnDENVytMOgQrQNe823z0R8M8pQ6/qJ+Abnag=";
+  npmDepsHash = "sha256-qXz8qaC3taTsk4r68fldGkKNgLNBeflwe5nII3SNUgE=";
 
   meta = {
     mainProgram = "vtsls";
