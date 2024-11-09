b12aa49a285de63c16ca8b1be69337dfcd48c9ee
Ratson <github@ratson.name>
2024-11-09 11:39:36 +0800

    feat(release-it): update to v17.10.0

--- nix/packages/release-it.nix
+++ nix/packages/release-it.nix
@@ -2,17 +2,16 @@
 
 let
   pname = "release-it";
-  version = "17.6.0";
+  version = "17.10.0";
 in
 buildNpmPackage (outputs.lib.npmArgs {
   inherit fetchurl lib pname version;
 
-  hash = "sha256-vN2A11mpa428o9ZitbM8NhX8gihEErbyNVTiSCUNG+U=";
+  hash = "sha256-EHPx4FcCKvshlQG1Gm61/TbqXDrixyFbfict6hkEbjw=";
   lock.url = "https://raw.githubusercontent.com/release-it/release-it/${version}/package-lock.json";
-  lock.hash = "sha256-IBksAmFkZEK/sFLhHK7YF7A2e53MzBR8N9M1hMkffjM=";
+  lock.hash = "sha256-EP14K+4dj7HZdmjiobnYG3GehnXfllBVdhXp4/NN/SQ=";
 
-  npmDepsHash = "sha256-LUJbbKws30B+AeGaUyx9gA532bRNse4AFixA1htxUMc=";
-  npmFlags = [ "--legacy-peer-deps" ];
+  npmDepsHash = "sha256-Po3QbRdVmvNaizIlfBvX5I7DL8k0G9Lc4L0gkpq+qfQ=";
 
   meta = {
     mainProgram = "release-it";
