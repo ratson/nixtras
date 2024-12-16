5c5f38852ff661139b1d545fb23218314277b088
Ratson <github@ratson.name>
2024-12-16 13:46:35 +0800

    feat(android-studio): update canary to v2024.3.1.5

--- nix/packages/android-studio-canary.nix
+++ nix/packages/android-studio-canary.nix
@@ -19,6 +19,6 @@ in
 mkStudio {
   channel = "canary";
   pname = "android-studio-canary";
-  version = "2024.3.1.4";
-  sha256Hash = "sha256-UJI7ouYPydOj0YE8SYN2UP+V1LXMt6QdjIwsq9D2mtA=";
+  version = "2024.3.1.5";
+  sha256Hash = "sha256-qBRb60F/3qFmX9k1Z0WsQHx5imACIvLB+YaVQbFFWpg=";
 }
